04ad83dec0568b76b55f8ad299a8e9bbee166ede
Sankar Srinivasan <petra.srini@gmail.com>
2026-04-08 19:39:58 +0530

    Create watcher.js

---
+++ src/watcher.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+function watchFile(file, callback) {
+  let timeout;
+
+  fs.watch(file, () => {
+    clearTimeout(timeout);
+
+    // debounce (important in real usage)
+    timeout = setTimeout(() => {
+      console.clear();
+      callback();
+    }, 200);
+  });
+}
+
+module.exports = { watchFile };
